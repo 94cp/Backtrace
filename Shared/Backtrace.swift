e994db07c0f73c7f86697a100d83d75ee26d9508
cp110 <1107223894@qq.com>
2021-02-25 20:19:53 +0800

    Update Backtrace.swift

--- Shared/Backtrace.swift
+++ Shared/Backtrace.swift
@@ -7,6 +7,37 @@
 
 import Foundation
 
+#if arch(i386)
+
+typealias THREAD_STATE_FLAVOR = x86_THREAD_STATE
+let THREAD_STATE_COUNT  = x86_THREAD_STATE_COUNT
+let __framePointer      = __ebp
+
+#elseif arch(x86_64)
+
+let THREAD_STATE_FLAVOR = x86_THREAD_STATE64
+let THREAD_STATE_COUNT  = x86_THREAD_STATE64_COUNT
+let __framePointer      = __rbp
+
+#elseif arch(arm)
+
+let THREAD_STATE_FLAVOR = ARM_THREAD_STATE
+let THREAD_STATE_COUNT  = ARM_THREAD_STATE_COUNT
+let __framePointer      = __r[7]
+
+#elseif arch(arm64)
+
+let THREAD_STATE_FLAVOR = ARM_THREAD_STATE64
+let THREAD_STATE_COUNT  = Mach.ARM_THREAD_STATE64_COUNT//MemoryLayout<arm_thread_state64_t>.size/MemoryLayout<UInt32>.size
+typealias __framePointer      = __fp
+
+#else
+
+#error ("Current CPU Architecture is not supported")
+
+#endif
+
+
 struct Backtrace {
     static var main_thread_t: mach_port_t?
     
@@ -14,21 +45,5 @@ struct Backtrace {
         main_thread_t = mach_thread_self()
     }
     
-    static func callstack(_ thread: Thread) {
-        
-    }
     
-    static func mach_callstack(_ thread: thread_t) {
-        let stackSize: Int32 = 128
-        let addrs = UnsafeMutablePointer<UnsafeMutableRawPointer?>.allocate(capacity: Int(stackSize))
-        defer {
-            addrs.deallocate()
-        }
-        
-        let frameCount = mach_backtrace(thread, addrs, stackSize)
-    }
-    
-//    static func machThread(from thread: Thread) -> thread_t {
-//        
-//    }
 }
